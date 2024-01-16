930a94acbc77b77fcc1e611c8d158dff95e96429
Yuma Gunawan <yumaanugrahviryagunawan@bangunindo.com>
2024-01-16 14:20:43 +0800

    change python client behavior

--- py_client/client.py
+++ py_client/client.py
@@ -8,15 +8,18 @@ def run():
         num2 = float(input('Enter second number : '))
         with grpc.insecure_channel("localhost:8080") as channel:
             stub = calculator_pb2_grpc.CalculationStub(channel)
+            
             add = stub.Addition(calculator_pb2.NumRequest(num_a=num1, num_b=num2))
+            print("Addition : " + remove_trailing_zero(str(add.result)))
+            
             sub = stub.Substraction(calculator_pb2.NumRequest(num_a=num1, num_b=num2))
+            print("Substraction : " + remove_trailing_zero(str(sub.result)))
+            
             mul = stub.Multiplication(calculator_pb2.NumRequest(num_a=num1, num_b=num2))
+            print("Multiplication : " + remove_trailing_zero(str(mul.result)))
+            
             div = stub.Division(calculator_pb2.NumRequest(num_a=num1, num_b=num2))
-        
-        print("Addition : " + remove_trailing_zero(str(add.result)))
-        print("Substraction : " + remove_trailing_zero(str(sub.result)))
-        print("Multiplication : " + remove_trailing_zero(str(mul.result)))
-        print("Division : " + remove_trailing_zero(str(div.result)))
+            print("Division : " + remove_trailing_zero(str(div.result)))
 
     except ValueError:
         print('Please input only number')
